f4d37d561f335f9d597106e5688335838bd73b50
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2018-10-25 16:25:17 -0700

    Refactored the script

--- idp_design_pattern_one/idp_pattern_1/install.sh
+++ idp_design_pattern_one/idp_pattern_1/install.sh
@@ -1,20 +1,19 @@
 cd config
 rm -rf edge.json
-mvn process-resources -Ptest -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.options=create 
-mvn install -Ptest -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.options=create 
+mvn install -Ptest -Dorg=$1 -Denv=$2 -Dusername=$3 -Dpassword=$4 -Dapigee.config.options=update 
 cd ../sharedflow
 rm -rf target
-mvn install -Ptest -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.options=create 
+mvn install -Ptest -Dorg=$1 -Denv=$2 -Dusername=$3 -Dpassword=$4 -Dapigee.config.options=update 
 cd ../proxy
 rm -rf target
-mvn install -Ptest -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.options=create 
+mvn install -Ptest -Dorg=$1 -Denv=$2 -Dusername=$3 -Dpassword=$4 -Dapigee.config.options=update 
 cd ../protected_api_proxy
 rm -rf target
-mvn install -Ptest -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.options=create 
+mvn install -Ptest -Dorg=$1 -Denv=$2 -Dusername=$3 -Dpassword=$4 -Dapigee.config.options=update 
 cd ../app_config
 rm -rf edge.json
-mvn process-resources -Ptest -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.options=create 
-mvn install -Ptest -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.options=create
-mvn apigee-config:exportAppKeys -Ptest  -Dorg=$1 -Dusername=$2 -Dpassword=$3 -Dapigee.config.exportDir=./app_info
+mvn process-resources -Ptest -Dorg=$1 -Denv=$2 -Dusername=$3 -Dpassword=$4 -Dapigee.config.options=update 
+mvn install -Ptest -Dorg=$1 -Denv=$2 -Dusername=$3 -Dpassword=$4 -Dapigee.config.options=update
+mvn apigee-config:exportAppKeys -Ptest -Dorg=$1 -Denv=$2 -Dusername=$3 -Dpassword=$4 -Dapigee.config.exportDir=./app_info
 cat ./app_info/devAppKeys.json
 cd ..
